223ec69804a4793769a5b6bc00e1be44d7a06abf
vortex41 <wojtex5@o2.pl>
2015-03-28 12:26:09 +0100

    Pusty projekt

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Wojciech Paluch <wojtex5 at o2.pl>
+ *
+ * Created on 28 marca 2015, 12:02
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
